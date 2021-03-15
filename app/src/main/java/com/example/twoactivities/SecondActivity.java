ba4e8de25bfa53419de74bd9cd53447e96aefd1a
bc-dipesh <bcdipeshwork@gmail.com>
2021-03-15 23:22:30 +0545

    FEAT: Use Intent to display data
    Get intent and display relevant text passage according to the intent extra

--- app/src/main/java/com/example/twoactivities/SecondActivity.java
+++ app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -1,17 +1,30 @@
 package com.example.twoactivities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private TextView mTextPassage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        mTextPassage = findViewById(R.id.text_passage);
         Intent intent = getIntent();
+        int textPassageNumber = intent.getIntExtra(MainActivity.TEXT_PASSAGE_NO, 1);
+
+        if (textPassageNumber == 1) {
+            mTextPassage.setText(R.string.text_passage_one);
+        } else if (textPassageNumber == 2) {
+            mTextPassage.setText(R.string.text_passage_two);
+        } else if (textPassageNumber == 3) {
+            mTextPassage.setText(R.string.text_passage_three);
+        }
 
     }
 }
