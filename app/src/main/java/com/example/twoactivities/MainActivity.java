7834f4d30394c5d962457c45a6795972c93d5934
bc-dipesh <bcdipeshwork@gmail.com>
2021-03-15 23:04:10 +0545

    FEAT: Add three ButtonView
    Add three ButtonView and implement onClick method for them

--- app/src/main/java/com/example/twoactivities/MainActivity.java
+++ app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.twoactivities;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    public static final String TEXT_PASSAGE_NO = "com.example.twoactivities.extra.TEXT_PASSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void launchSecondActivity(View view) {
+        Intent intent = new Intent(SecondActivity.class);
+        int id = view.getId();
+        int textPassageNumber = 1;
+
+        if (id == R.id.button_two) {
+            textPassageNumber = 2;
+        } else if (id == R.id.button_three) {
+            textPassageNumber = 3;
+        }
+
+        intent.putExtra(TEXT_PASSAGE_NO, textPassageNumber);
+        startActivity(intent);
     }
 }
