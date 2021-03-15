75da60ac1b35db64eb36cb4440e765cdbb2c2e0f
bc-dipesh <bcdipeshwork@gmail.com>
2021-03-15 23:51:29 +0545

    FEAT: Implement countUp
    Implement countUp method that increase count on button_count click

--- app/src/main/java/com/example/twoactivities/MainActivity.java
+++ app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.twoactivities;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mCount;
+    private int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mCount = findViewById(R.id.count);
+        count = Integer.parseInt(mCount.getText().toString());
+    }
+
+    public void sayhello(View view) {
+
+    }
+
+    public void countUp(View view) {
+        ++count;
+        mCount.setText("" + count);
     }
 }
